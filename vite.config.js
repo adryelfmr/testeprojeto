f4541cd472d9121e097f5c5c1332e625a2e8dae8
adryel <adryel_fmr@hotmail.com>
2024-10-22 11:47:05 -0300

    f

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/projetoeng/", // Substitua pelo nome do seu repositório
+  base: "/testeprojeto", // Substitua pelo nome do seu repositório
   build: {
     target: "esnext",
     outDir: "dist", // Pasta onde o Vite colocará os arquivos gerados
